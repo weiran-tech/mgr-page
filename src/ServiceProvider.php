b3c998a7c90477a3aca2ee9c2765b2e701d328ab
duoli <zhaody901@gmail.com>
2025-03-22 10:21:05 +0800

    refactor(weiran/mgr-page): 重构服务提供者并优化资源配置
    - 将资源配置从 boot 方法中移至专门的 registerConfiguration 方法
    - 使用类型声明和空行提高代码可读性
    - 优化代码结构，使 register 方法更加清晰

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -23,36 +23,16 @@ class ServiceProvider extends WeiranServiceProvider
      * @return void
      * @throws ModuleNotFoundException
      */
-    public function boot()
+    public function boot(): void
     {
         parent::boot('weiran.mgr-page');
-
-        // 注册 api 文档配置
-        $this->publishes([
-            __DIR__ . '/../resources/views/vendor/pagination-layui.blade.php' => resource_path('views/vendor/pagination/layui.blade.php'),
-        ], 'weiran');
-        $this->publishes([
-            // 需要从项目中反向复制的页面
-            __DIR__ . '/../resources/libs/boot/app.min.js'        => public_path('assets/libs/boot/app.min.js'),
-            __DIR__ . '/../resources/libs/boot/style.css'         => public_path('assets/libs/boot/style.css'),
-            // editor
-            __DIR__ . '/../resources/libs/boot/wangeditor@5.1.js' => public_path('assets/libs/boot/wangeditor@5.1.js'),
-            // 编辑器
-            __DIR__ . '/../resources/libs/jquery/backstretch'     => public_path('assets/libs/jquery/backstretch'),
-            __DIR__ . '/../resources/libs/vue'                    => public_path('assets/libs/vue'),
-            __DIR__ . '/../resources/libs/easy-web/'              => public_path('assets/libs/easy-web/'),
-            __DIR__ . '/../resources/libs/jquery/data-tables/'    => public_path('assets/libs/jquery/data-tables/'),
-            __DIR__ . '/../resources/libs/layui/'                 => public_path('assets/libs/layui/'),
-            __DIR__ . '/../resources/images/'                     => public_path('assets/images/'),
-            __DIR__ . '/../resources/font/'                       => public_path('assets/font/'),
-        ], 'weiran-assets');
     }
 
     /**
      * Register the module services.
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->app->register(Http\MiddlewareServiceProvider::class);
         $this->app->register(Http\RouteServiceProvider::class);
@@ -62,6 +42,8 @@ class ServiceProvider extends WeiranServiceProvider
         $this->registerSchedule();
 
         $this->registerForm();
+
+        $this->registerConfiguration();
     }
 
     public function provides(): array
@@ -94,5 +76,31 @@ class ServiceProvider extends WeiranServiceProvider
 
             return $form->setSessionStore($app['session.store']);
         });
+    }
+
+
+    private function registerConfiguration(): void
+    {
+        // 注册 api 文档配置
+        $this->publishes([
+            __DIR__ . '/../resources/views/vendor/pagination-layui.blade.php' => resource_path('views/vendor/pagination/layui.blade.php'),
+        ], 'weiran');
+
+        // 注册静态资源
+        $this->publishes([
+            // 需要从项目中反向复制的页面
+            __DIR__ . '/../resources/libs/boot/app.min.js'        => public_path('assets/libs/boot/app.min.js'),
+            __DIR__ . '/../resources/libs/boot/style.css'         => public_path('assets/libs/boot/style.css'),
+            // editor
+            __DIR__ . '/../resources/libs/boot/wangeditor@5.1.js' => public_path('assets/libs/boot/wangeditor@5.1.js'),
+            // 编辑器
+            __DIR__ . '/../resources/libs/jquery/backstretch/'    => public_path('assets/libs/jquery/backstretch/'),
+            __DIR__ . '/../resources/libs/vue/'                   => public_path('assets/libs/vue/'),
+            __DIR__ . '/../resources/libs/easy-web/'              => public_path('assets/libs/easy-web/'),
+            __DIR__ . '/../resources/libs/jquery/data-tables/'    => public_path('assets/libs/jquery/data-tables/'),
+            __DIR__ . '/../resources/libs/layui/'                 => public_path('assets/libs/layui/'),
+            __DIR__ . '/../resources/images/'                     => public_path('assets/images/'),
+            __DIR__ . '/../resources/font/'                       => public_path('assets/font/'),
+        ], 'weiran-assets');
     }
 }
